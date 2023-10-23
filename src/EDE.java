3167b49b3b824a20405f972f59e28d6f1c19c639
Alex3450 <alejandroexposito.ae@gmail.com>
2023-10-23 16:49:43 +0200

    V2_Implementado algoritmo de obtención del mayor de dos números

--- src/EDE.java
+++ src/EDE.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        int n1, n2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserte primer número");
+        n1=sc.nextInt();
+        System.out.println("Inserte segundo número");
+        n2=sc.nextInt();
+        System.out.println("Inserte tercer número");
+    }
 }
